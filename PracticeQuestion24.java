f791965a00c33f74bbd67685886a055c4ed5799f
Joma Oliver <Oliverjoma03+1@gmail.com>
2025-04-25 13:36:02 -0400

    Initial sharing of project

---
+++ PracticeQuestion24.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class PracticeQuestion24 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PracticeQuestion24
+{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class PracticeQuestion24
+     */
+    public PracticeQuestion24()
+    {
+        String a = "Hello";
+        String b = "World";
+        if(a.equals(b)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+        
+        String c = "Hello";
+        String d = c;
+        if(c.equals(d)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
---
+++ PracticeQuestion25.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+/**
+ * Write a description of class PracticeQuestion25 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class PracticeQuestion25
+{
+    // instance variables - replace the example below with your own
+    
+    /**
+     * Constructor for objects of class PracticeQuestion25
+     */
+    public PracticeQuestion25()
+    {
+        
+    }
+    
+    public void fever(ArrayList<double> temperatures) {
+        // Question 25
+        int count;
+        double max = 0.0;
+        for(double temp : temperatures) {
+            if(temp > 37.5) { count++; }
+            if(max < temp) {max = temp;}
+        }
+        System.out.println("fevers #:" +count);
+        System.out.println("max temp:" +max);
+        
+        // Question 26
+        System.out.println(Collections.max(temperatures));
+        
+        // Question 27
+        Collections.sort(temperatures);
+        System.out.println(temperatures.last());
+        
+    }
+}
